3f852e88a1942364980c8ca0333a76842c7a1420
Arka Ghosh <53995301+arkag30@users.noreply.github.com>
2023-09-23 16:12:28 +0530

    changes

--- models/t5model.py
+++ models/t5model.py
@@ -10,7 +10,7 @@ class t5model:
         input_ids = self.tokenizer.encode("summarize: " + self.paper_text, return_tensors="pt", max_length=1024, truncation=True)
 
         # Generate the summary
-        summary_ids = self.model.generate(input_ids, max_length=2000, min_length=500, length_penalty=2.0, num_beams=4, early_stopping=True)
+        summary_ids = self.model.generate(input_ids, max_length=500, min_length=100, length_penalty=2.0, num_beams=4, early_stopping=True)
 
         # Decode and print the generated summary
         summary = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
