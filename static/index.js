c77b2c9770e5b72427cfa8a93c46d996cec0770a
Adrija Acharyya <adrijaacharyya26@gmail.com>
2023-09-23 13:23:37 +0530

    Moded radiobuttons , added options

--- static/index.js
+++ static/index.js
@@ -1,3 +1,38 @@
+submit = document.querySelector('.submit');
+list = document.querySelector('.sum-more');
+
+tasks = document.getElementsByName('rd');
+let selectedTask;
+
+
+
+const handleSubmit = (e) => {
+    for(const selected of tasks) {
+        if(selected.checked) {
+            selectedTask = selected.value;
+            break;
+        }
+    }
+    
+}
+
+tasks.forEach(function(input) {
+    input.addEventListener("click", function() {
+        let inputValue = input.value;
+        if (inputValue === 'summarize') {
+            list.style.display = 'block';
+        }
+        if (inputValue === 'grammar') {
+            list.style.display = 'none';
+        }
+    });
+});
+  
+
+submit.addEventListener('click', handleSubmit);
+
+
+//Text to speech
 output = document.getElementById('span').innerText
 speech = new SpeechSynthesisUtterance(output)
 
